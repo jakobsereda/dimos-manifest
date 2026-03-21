5b8c8a3261a04500d9c5e5a1ab4c9f183cd82cdb
jakobsereda <jakobsereda@gmail.com>
2026-03-21 15:56:03 -0700

    chore: script outline

--- setup.sh
+++ setup.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+WORKSPACE="$(pwd)"
+
+# TODO: sanity check correct installed packages
+# TODO: init bitbake
+# TODO: add layers
+# TODO: append to local conf? this is a bad temp fix, the details should be in dimos layer...?
+
 cat "test"
