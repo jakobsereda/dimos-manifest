4264b4d7d38ee7fa072e3537abccbb878b032375
jakobsereda <jakobsereda@gmail.com>
2026-03-24 19:32:26 -0700

    feat: try sourcing in script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,15 @@
 WORKSPACE="$(pwd)"
 
 # TODO: sanity check correct installed packages
-# TODO: init bitbake
+# check if repo is installed
+# dpkg -l repo
+# if [[ $? -ne 0 ]] ; then
+# 	exit 1
+# fi
+
+echo -e "initializing yocto build environment...\n"
+source "${WORKSPACE}/layers/openembedded-core/oe-init-build-env"
+
 # TODO: add layers
 # TODO: append to local conf? this is a bad temp fix, the details should be in dimos layer...?
 
