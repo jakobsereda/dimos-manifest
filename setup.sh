bf1827f8d4dfea64fde89818a8ec45675d45caf1
jakobsereda <jakobsereda@gmail.com>
2026-03-24 19:37:35 -0700

    feat: this could maybe work for bitbake layers?

--- setup.sh
+++ setup.sh
@@ -2,17 +2,13 @@
 
 WORKSPACE="$(pwd)"
 
-# TODO: sanity check correct installed packages
-# check if repo is installed
-# dpkg -l repo
-# if [[ $? -ne 0 ]] ; then
-# 	exit 1
-# fi
-
-echo -e "initializing yocto build environment...\n"
 source "${WORKSPACE}/layers/openembedded-core/oe-init-build-env"
 
-# TODO: add layers
+for repo in "${WORKSPACE}/layers"
+do
+	bitbake-layers add-layer "${WORKSPACE}/layers/${repo}"
+done
+
 # TODO: append to local conf? this is a bad temp fix, the details should be in dimos layer...?
 
 cat "test"
