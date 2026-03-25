8305ad4e0be454f8b5c31f740557c6234672e63b
jakobsereda <jakobsereda@gmail.com>
2026-03-24 19:49:10 -0700

    feat: try this looping strat instead

--- setup.sh
+++ setup.sh
@@ -4,9 +4,23 @@ WORKSPACE="$(pwd)"
 
 source "${WORKSPACE}/layers/openembedded-core/oe-init-build-env"
 
-for repo in "${WORKSPACE}/layers"
-do
-	bitbake-layers add-layer "${WORKSPACE}/layers/${repo}"
+declare -a layers=(
+	"${WORKSPACE}/layers/meta-openembedded/meta-oe"
+ 	"${WORKSPACE}/layers/meta-openembedded/meta-python"
+ 	"${WORKSPACE}/layers/meta-openembedded/meta-networking"
+ 	"${WORKSPACE}/layers/meta-openembedded/meta-gnome"
+ 	"${WORKSPACE}/layers/meta-openembedded/meta-webserver"
+ 	"${WORKSPACE}/layers/meta-openembedded/meta-multimedia"
+ 	"${WORKSPACE}/layers/meta-clang"
+ 	"${WORKSPACE}/layers/meta-flutter"
+ 	"${WORKSPACE}/layers/meta-st-stm32mp"
+ 	"${WORKSPACE}/layers/meta-st-openstlinux"
+ 	"${WORKSPACE}/layers/meta-flutter/meta-flutter-apps"
+ 	"${WORKSPACE}/layers/meta-dimos"
+)
+
+for layer in "${layers[@]}" ; do
+	bitbake-layers add-layer "$layer"
 done
 
 # TODO: append to local conf? this is a bad temp fix, the details should be in dimos layer...?
